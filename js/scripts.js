ab862b3d3fe0fb670fc5fc199fd7d830dc469503
tirtzahux <tirtzahsondhelm1@gmail.com>
2024-09-15 13:15:32 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log ("Hi, I'm Tirtzah. Welcome to my portfolio website!");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';       
+    } else {
+        x.className = 'navtoggle';
+    }
+}
