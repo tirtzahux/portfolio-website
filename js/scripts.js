da55ee3d75701bd920941550521ab3ba002be9f6
tirtzahux <tirtzahsondhelm1@gmail.com>
2024-09-18 20:28:58 +0300

    Update js\scrpits.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 console.log ("Hi, I'm Tirtzah. Welcome to my portfolio website!");
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
+    if (x.className === 'navtoggle' && $(window).width() < 640) {
         x.className += ' responsive';       
     } else {
         x.className = 'navtoggle';
